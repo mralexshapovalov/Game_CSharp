4f5abf4e8d6760720ebc1f9f64de8c0e2014f518
Alexandr Shapovalov <a.a.shapovalov@bk.ru>
2023-06-13 22:16:49 +0300

    CS

--- SnakeGame/Images.cs
+++ SnakeGame/Images.cs
@@ -8,11 +8,11 @@ namespace SnakeGame
     public  static class Images
     {
         public readonly static ImageSource Empty = LoadImage("Empty.png");
-        public readonly static ImageSource Body = LoadImage("body_horizontal.png");
+        public readonly static ImageSource Body = LoadImage("Body.png");
         public readonly static ImageSource Head = LoadImage("Head.png");
-        public readonly static ImageSource Food = LoadImage("Food.png");
+        public readonly static ImageSource Food = LoadImage("apple.png");
         public readonly static ImageSource DeadBody = LoadImage("DeadBody.png");
-        public readonly static ImageSource DeadHead = LoadImage("");
+        public readonly static ImageSource DeadHead = LoadImage("DeadHead.png");
 
         private static ImageSource LoadImage(string fillName)
         {
